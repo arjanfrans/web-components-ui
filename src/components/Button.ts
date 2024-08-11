effc98bd872f802a6bd8595d3f47d16d15835d71
Arjan Frans <arjanfrans.com@gmail.com>
2024-08-11 13:33:49 +0000

    update button touch behaviour

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -32,6 +32,7 @@ export class Button extends HTMLElement {
         text-align: center;
         transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1), box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1), border-color 250ms cubic-bezier(0.4, 0, 0.2, 1), color 250ms cubic-bezier(0.4, 0, 0.2, 1);
         white-space: nowrap;
+        touch-action: manipulation; /* Helps with touch interactions */
       }
       
       :host([variant="filled"]) .button {
@@ -39,7 +40,8 @@ export class Button extends HTMLElement {
         color: var(--semantic-text-inverted);
       }
 
-      :host([variant="filled"]:hover) .button {
+      :host([variant="filled"]:hover) .button,
+      :host([variant="filled"]:active) .button {
         background-color: var(--semantic-background-hover);
         color: var(--semantic-text-inverted);
         box-shadow: 
@@ -54,7 +56,8 @@ export class Button extends HTMLElement {
         color: var(--semantic-text-highlight);
       }
 
-      :host([variant="outlined"]:hover) .button {
+      :host([variant="outlined"]:hover) .button,
+      :host([variant="outlined"]:active) .button {
         border-color: var(--semantic-stroke-hover);
         color: var(--semantic-text-hover);
         background-color: rgba(var(--semantic-background-highlight_rgb), 0.1);
@@ -82,12 +85,10 @@ export class Button extends HTMLElement {
     shadow.appendChild(style);
 
     const label = new Typography();
-
     label.display = "button";
 
     // Create a slot for button content
     const slot = document.createElement("slot");
-
     label.append(slot);
     this.button.appendChild(label);
 
@@ -100,6 +101,22 @@ export class Button extends HTMLElement {
     if (!this.hasAttribute("size")) {
       this.setAttribute("size", "medium");
     }
+
+    // Add touch event listeners
+    this.button.addEventListener(
+      "touchstart",
+      this.handleTouchStart.bind(this),
+    );
+    this.button.addEventListener("touchend", this.handleTouchEnd.bind(this));
+    this.button.addEventListener("touchcancel", this.handleTouchEnd.bind(this));
+  }
+
+  private handleTouchStart() {
+    this.button.classList.add("button-active");
+  }
+
+  private handleTouchEnd() {
+    this.button.classList.remove("button-active");
   }
 
   // Observe changes to 'variant' and 'size' attributes
